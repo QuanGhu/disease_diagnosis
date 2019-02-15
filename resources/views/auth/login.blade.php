5bc1af5191c153279cb637a0f1a5180d3c62bf26
Ari Putra <ariteknologi@gmail.com>
2019-02-16 02:42:28 +0700

    change auth layout

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,94 +1,47 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
-    <style>
-        .website-name {
-            height: 200px;
-            text-align: center;
-            padding: 25px;
-            background-image: url('assets/img/soil_bg.jpg');
-            background-size: cover;
-            background-repeat: no-repeat;
-            width: 100%;
-            background-position: center;
-            color: #fff;
-        }
-    </style>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Sistem Pakar</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <link rel="stylesheet" href="{{ asset('awkawrd/css/bootstrap.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('awkawrd/css/font-awesome.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('awkawrd/css/ionicons.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('awkawrd/css/AdminLTE.min.css') }}">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
 </head>
-<body>
-    <div class="header">
-        <div class="website-name">
-            <h3>Sistem Pakar Diagnosa Penyakit</h3>
-        </div>
-        <nav class="navbar navbar-default">
-            <div class="container-fluid">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span> 
-                    </button>
-                </div>
-                <div class="collapse navbar-collapse" id="myNavbar">
-                    <ul class="nav navbar-nav">
-                        <li><a href="{{ route('login') }}"><i class="fa fa-sign-in" aria-hidden="true"></i> Masuk</a></li>
-                        <li><a href="{{ route('login.admin.view') }}"><i class="fa fa-sign-in" aria-hidden="true"></i> Masuk Admin</a></li>
-                        <li><a href="{{ route('register.view') }}"><i class="fa fa-user-plus" aria-hidden="true"></i> Daftar</a></li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    </div>
-    <div class="content">
-        @if (session()->has('danger'))
-            <div class="alert alert-danger">
-                <strong>Error!</strong>
-                {{ session()->get('danger') }}
-            </div>
-        @endif
-    
-        @if ($errors->any())
-            <div class="alert alert-danger">
-                <ul>
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
-        @endif
-        @if (session()->has('success'))
-            <div class="alert alert-success">
-                <strong>Success!</strong>
-                {{ session()->get('success') }}
-            </div>
-        @endif
-        <div class="container">
-            <div class="row">
-                <div class="col-md-4 col-md-offset-4">
-                    <div class="login-box">
-                        <h3 class="signin-title-secondary">Silakan masuk untuk melanjutkan.</h3>
-                        {!! Form::open(['id' => 'form','route' => 'login.user']) !!}
-                            <div class="form-group">
-                                <input name="email" type="text" class="form-control" placeholder="Masukan email anda">
-                            </div>
-                            <div class="form-group mg-b-50">
-                                <input name="password" type="password" class="form-control" placeholder="Masukan Password Anda">
-                            </div>
-                            <button type="submit" class="btn btn-primary btn-block btn-signin">Masuk</button>
-                        {!! Form::close() !!}
-                        <p class="mg-b-0">Belum punya akun? <a href="{{ route('register.view') }}">Daftar disini</a></p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+<body class="hold-transition login-page">
+<div class="login-box">
+  <div class="login-logo">
+    <a href="#">Sistem Pakar Diagnosa Penyakit</a>
+  </div>
+  <!-- /.login-logo -->
+  <div class="login-box-body">
+    <p class="login-box-msg">Masuk untuk mulai</p>
 
-    <script src="{{ asset('assets/js/jquery.min.js') }}"></script>
-    <script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
+    {!! Form::open(['id' => 'form','route' => 'login.user']) !!}
+        <div class="form-group">
+            <input name="email" type="text" class="form-control" placeholder="Masukan email anda">
+        </div>
+        <div class="form-group mg-b-50">
+            <input name="password" type="password" class="form-control" placeholder="Masukan Password Anda">
+        </div>
+        <button type="submit" class="btn btn-primary btn-block btn-signin">Masuk</button>
+    {!! Form::close() !!}
+    <br>
+    <a href="{{ route('register.view') }}" class="text-center">Daftarkan Akun</a>
+    <br>
+    <a href="{{ route('login.admin.view') }}" class="text-center">Admin Masuk Disini</a>
+
+  </div>
+  <!-- /.login-box-body -->
+</div>
+<!-- /.login-box -->
+
+<script src="{{ asset('awkawrd/js/jquery.min.js') }}"></script>
+<script src="{{ asset('awkawrd/js/bootstrap.min.js') }}"></script>
 </body>
 </html>
+
